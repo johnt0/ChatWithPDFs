de60d7db406062b714965b2c82b6d7428c49aca4
johnt0 <52798743+johnt0@users.noreply.github.com>
2023-07-19 18:27:50 -0400

    finish

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ from langchain.vectorstores import FAISS
 from langchain.llms import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
+from htmlTemplates import css, bot_template, user_template
+
 
 def get_pdf_text(pdf_docs):
   text = ""
@@ -40,19 +42,38 @@ def get_conversation_chain(vectorstore):
   )
   return conversation_chain
 
+def handle_userinput(input):
+  response = st.session_state.conversation({'question': input})
+  st.session_state.chat_history = response['chat_history']
+
+  for i, message in enumerate(st.session.chat_history):
+    if i & 2 == 0:
+      st.write(user_template.replace('{{MSG}}', message.content), unsafe_allow_html=True)
+    else:
+      st.write(bot_template.replace('{{MSG}}', message.content), unsafe_allow_html=True)
+
 
 def main():
   #load api keys
   load_dotenv()
   #basic configuration of the page
   st.set_page_config(page_title="Chat with multiple PDFs", page_icon=":books:")
+  st.write(css, unsafe_allow_html=True)
 
+  #initialization
   if "conversation" not in st.session_state:
     st.session_state.conversation = None
-    
+  if "chat_history" not in st.session_state:
+    st.session_state.chat_history = None
+  
   st.header("Chat with multiple PDFs :books:")
-  st.text_input("Ask a question about your documents:")
+  
+  user_question = st.text_input("Ask a question about your documents:")
+  if user_question:
+    handle_userinput(user_question)
 
+  st.write(user_template, unsafe_allow_html=True)
+  st.write(bot_template, unsafe_allow_html=True)
   #create sidebar to upload files
   with st.sidebar:
     st.subheader("Your documents")
